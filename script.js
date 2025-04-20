8f8eec56339f9baa974f3dfe423dcc2499022243
Nihita Lele <nihitalele@gmail.com>
2025-04-20 23:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+// team is a reference to players
+const team = players;
+
+// team1 is a copy of players (shallow copy using spread operator)
+const team1 = [...players];
+
+// cap1 is a copy of person (shallow copy using spread operator)
+const cap1 = { ...person };
+
 window.players = players;
 window.person = person;
 window.team = team;
